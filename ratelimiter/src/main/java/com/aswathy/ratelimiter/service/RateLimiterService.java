a772291ee0b495399486199d77a0c27b35a906ac
aswathyKR <aswathykrmail@gmail.com>
2026-02-10 12:25:03 +0530

    feat: add multi-tier rate limiting logic

--- ratelimiter/src/main/java/com/aswathy/ratelimiter/service/RateLimiterService.java
+++ ratelimiter/src/main/java/com/aswathy/ratelimiter/service/RateLimiterService.java
@@ -11,30 +11,42 @@ import java.util.concurrent.ConcurrentHashMap;
 public class RateLimiterService {
 
     private final Map<String, TokenBucket> buckets = new ConcurrentHashMap<>();
-    private final int capacity;
-    private final int refillRate;
+
+    // These defaults come from application.properties (Capacity=1)
+    private final int defaultCapacity;
+    private final int defaultRefillRate;
 
     public RateLimiterService(
-            @Value("${ratelimit.capacity}") int capacity,
-            @Value("${ratelimit.refill-rate}") int refillRate
+            @Value("${ratelimit.capacity}") int defaultCapacity,
+            @Value("${ratelimit.refill-rate}") int defaultRefillRate
     ) {
-        this.capacity = capacity;
-        this.refillRate = refillRate;
-
-        // --- DEBUG PRINT ---
-        System.out.println("==================================");
-        System.out.println("RATE LIMITER CONFIG LOADED:");
-        System.out.println("Capacity: " + capacity);
-        System.out.println("Refill Rate: " + refillRate);
-        System.out.println("==================================");
+        this.defaultCapacity = defaultCapacity;
+        this.defaultRefillRate = defaultRefillRate;
     }
 
     public boolean isAllowed(String apiKey) {
-        TokenBucket bucket = buckets.computeIfAbsent(apiKey, key -> createNewBucket());
+        // We pass the 'apiKey' into the createNewBucket method now!
+        TokenBucket bucket = buckets.computeIfAbsent(apiKey, this::createNewBucket);
         return bucket.tryConsume();
     }
 
-    private TokenBucket createNewBucket() {
-        return new TokenBucket(capacity, refillRate);
+    private TokenBucket createNewBucket(String apiKey) {
+        // --- BUSINESS LOGIC HERE ---
+
+        // Tier 1: Premium Users (Start with "prem-")
+        if (apiKey.startsWith("prem-")) {
+            System.out.println("Creating Premium Bucket for: " + apiKey);
+            return new TokenBucket(20, 2); // 20 tokens, 2 per second
+        }
+
+        // Tier 2: Free Users (Start with "free-")
+        if (apiKey.startsWith("free-")) {
+            System.out.println("Creating Free Bucket for: " + apiKey);
+            return new TokenBucket(5, 1);  // 5 tokens, 1 per second
+        }
+
+        // Tier 3: Everyone else (Uses the strict settings from properties file)
+        System.out.println("Creating Default Bucket for: " + apiKey);
+        return new TokenBucket(defaultCapacity, defaultRefillRate);
     }
 }
